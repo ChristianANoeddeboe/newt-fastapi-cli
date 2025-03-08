d0fed0b6b3a69252e94ff845822190fc709a4967
Christian <angercraft@gmail.com>
2025-03-08 11:54:47 +0100

    auto help for config command and make

--- vapor/commands/config/__init__.py
+++ vapor/commands/config/__init__.py
@@ -2,6 +2,6 @@ import typer
 
 from vapor.commands.config.init import app as init_app
 
-app = typer.Typer(invoke_without_command=True)
+app = typer.Typer(no_args_is_help=True)
 
 app.add_typer(init_app)
--- vapor/commands/make/__init__.py
+++ vapor/commands/make/__init__.py
@@ -2,6 +2,6 @@ import typer
 
 from .route import app as route_app
 
-app = typer.Typer()
+app = typer.Typer(no_args_is_help=True)
 
 app.add_typer(route_app)
